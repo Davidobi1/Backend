0158d78ecf498df64b6adbdcf3dffb972ab3f5fb
Davidobi1 <138148647+Davidobi1@users.noreply.github.com>
2024-06-10 11:47:48 +0000

    Add files via upload

---
+++ rockpaperscissors.py
@@ -0,0 +1,23 @@
+import random
+import time
+
+def play():
+    user = input("Lets play? 'r' for rock,'p' for paper, 's' scissors ")
+    computer = random.choice(['r', 'p', 's'])
+    time.sleep(0.5)
+    print(f"Computer's choice: {computer}")
+   
+    if user == computer:
+       return 'Its a tie'
+    
+    if is_win(user, computer):
+       return'Yay! You won'
+    
+    return'Yay! I won'
+
+def is_win(player, opponent):
+        
+        if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
+           return True
+      
+print(play())
